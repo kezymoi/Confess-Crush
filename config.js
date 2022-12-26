17bc5943ff52354acdc195631cc8feff9e8c9880
kezymoi <121419001+kezymoi@users.noreply.github.com>
2022-12-26 15:16:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 const CONFIG = {
     titleWeb: "Send to my Love",
-    introTitle: 'Tran Tuyet Giang',
+    introTitle: 'Hi Tran Tuyet Giang',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    btnIntro: 'HiHi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 8 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Vì cậu rất đẹp traiiii <33',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    mess: 'Mình biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
     messDesc: 'Thi xong mình đón cậu đi chơi nhaaaa.',
     btnAccept: 'Okiiiii lun <3', 
+    messlink: 'https://www.facebook.com/messages/t/100022786568289'
 }
