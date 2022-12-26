57734bdb9ad7ab2b4b2419087890a3389a771966
kezymoi <121419001+kezymoi@users.noreply.github.com>
2022-12-26 13:30:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Send to my Love",
+    introTitle: 'Tran Tuyet Giang',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
